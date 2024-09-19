e2900e52f89b46710c46dfb83e4eb349ba778f31
Mango Jelly Pudding <evan940415@gmail.com>
2024-09-19 10:14:46 +0800

    fix: remove temp files

--- docs/.vuepress/.temp/pages/zh-tw/studing/networks/2024.05.31.html.vue
+++
@@ -1,8 +0,0 @@
-<template><div><h1 id="_2024-05-31" tabindex="-1"><a class="header-anchor" href="#_2024-05-31"><span>2024.05.31</span></a></h1>
-<h2 id="_5-7-總結" tabindex="-1"><a class="header-anchor" href="#_5-7-總結"><span>5.7 總結</span></a></h2>
-<ul>
-<li>網際網路上有各式各樣與網路層相關的協定。包括資料段協定、IP 及相關的控制協定，如 ICMP、++ARP++ 和 <mark>DHCP</mark>。一種稱為 MPLS 的連接導向協定可承載 IP 封包跨越其他網路。用於++網路中的主要路由協定之一是 <mark>OSPF</mark>++，而用於++網路之間的路由協定為 <mark>BGP</mark>++。網際網路正快速地耗盡 IP 位址，所以新版 IP 協定，也就是 IPv6，已被發展出來並緩慢地佈建。</li>
-</ul>
-</div></template>
-
-
--- docs/.vuepress/.temp/redirect/map.js
+++
@@ -1,3 +0,0 @@
-export const redirectMap = {
-  "/index.html": "/en-us/index.html"
-};
