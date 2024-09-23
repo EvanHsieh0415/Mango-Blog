0c09b3a5e2d32cecc6625eaba6287a0fa3b27947
Mango <evan940415@gmail.com>
2024-09-23 09:37:18 +0800

    try new layout

---
+++ docs/.vuepress/layouts/testLayout.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import NormalPage from "vuepress-theme-hope/components/NormalPage.js"
+</script>
+
+<template>
+  <NormalPage>
+    <template #tocBefore> tocBefore </template>
+  </NormalPage>
+</template>
--- docs/zh-tw/test.md
+++ docs/zh-tw/test.md
@@ -1,5 +1,20 @@
 ---
-sidebar: false
-index: false
-article: false
+# sidebar: false
+# index: false
+# article: false
+layout: testLayout
 ---
+
+# h1-1
+
+## h2-1
+
+## h2-2
+
+### h3-1
+
+## h2-3
+
+### h3-2
+
+#### h1-1
